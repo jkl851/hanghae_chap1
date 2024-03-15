c5260b7125e8f07a233ec38f2586a7277309ddcf
mincheol <mincheol@douzone.com>
2024-03-15 22:58:27 +0900

    [1주차] 과제 C

--- packages/chapter1/src/a.js
+++ packages/chapter1/src/a.js
@@ -24,17 +24,11 @@ function Worker (health) {
 }
 
 function JuniorEngineer(health, intelligence) {
-  this._super(health);
+  Worker.call(this, health);
   this._intelligence = intelligence ?? 1;
-  if (this._intelligence > 10) {
-    this._isBornGenius = true;
-  }
+  this._isBornGenius = this._intelligence > 10;
 }
-//- 생성자 함수는 수정하지 마세요
 
-// 여기에 코드를 작성하세요
-// TO-DO
-//- 여기에 코드를 작성하세요
 Worker.prototype.getHealth = function () {
   return this._health;
 }
@@ -44,21 +38,12 @@ Worker.prototype.work = function () {
 }
 
 // 1번
-// JuniorEngineer.prototype = Object.create(Worker.prototype);
+JuniorEngineer.prototype = Object.create(Worker.prototype);
 
 // 2번
-JuniorEngineer.prototype = new Worker();
-
-// 1번
-// JuniorEngineer.prototype._super = function (health) {
-//   Worker.call(this, health);
-// }
+// JuniorEngineer.prototype = new Worker();
 
 // 2번
-JuniorEngineer.prototype._super = function () {
-  Worker.apply(this, arguments);
-}
-
 JuniorEngineer.prototype.getIntelligence = function () {
   return this._intelligence;
 }
@@ -69,7 +54,7 @@ JuniorEngineer.prototype.work = function () {
 }
 
 JuniorEngineer.prototype.isBornGenius = function () {
-  return this._isBornGenius ?? false;
+  return this._isBornGenius;
 }
 
 
@@ -97,17 +82,17 @@ JuniorEngineer.prototype.isBornGenius = function () {
  * - V8 엔진의 히든클래스 개념을 이해하고 이 개념을 응용하여 최적화 해보세요.
  * - ES 모듈시스템으로 바꾼뒤, 확장자를 .mjs로 변경한 뒤 실행해보세요. 최적화 결과가 같을까요?
  */
-// function main() {
-//   var startTime = performance.now();
-//   for (var i = 0; i < 10000000; i++) {
-//     new JuniorEngineer(10, Math.floor(Math.random() * 20)).isBornGenius();
-//   }
-//   var endTime = performance.now();
+function main() {
+  var startTime = performance.now();
+  for (var i = 0; i < 10000000; i++) {
+    new JuniorEngineer(10, Math.floor(Math.random() * 20)).isBornGenius();
+  }
+  var endTime = performance.now();
   
-//   console.log(endTime - startTime);
-// }
+  console.log(endTime - startTime);
+}
 
-// main();
+main();
 
 module.exports = {
   Worker,
