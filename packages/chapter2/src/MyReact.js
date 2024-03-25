ef14400353e8dc1902080e9b498dcda5ee1e15db
mincheol <mincheol@douzone.com>
2024-03-25 22:17:28 +0900

    [2주차] 과제C

--- packages/chapter2/src/MyReact.js
+++ packages/chapter2/src/MyReact.js
@@ -1,9 +1,25 @@
-import { createHooks } from "./hooks";
-import { render as updateElement } from "./render";
+import { createHooks } from "./hooks.js";
+import { render as updateElement, jsx } from "./render.js";
 
 function MyReact() {
-  const _render = () => {};
-  function render($root, rootComponent) {}
+  const __ = {
+    $root : null,
+    rootComponent : null,
+    prevNodes : null
+  }
+
+  const _render = () => {
+    const newNodes = __.rootComponent();
+    updateElement(__.$root, newNodes, __.prevNodes);
+    resetHookContext();
+    __.prevNodes = newNodes;
+  };
+  function render($root, rootComponent) {
+    __.$root = $root;
+    __.rootComponent = rootComponent;
+    __.prevNodes = null;
+    _render();
+  }
 
   const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
 
